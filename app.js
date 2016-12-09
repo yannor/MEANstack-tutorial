6b0e54d85f541a53ed32dfd9f9d62e3a9a0ba777
Yannick <yannick.servranckx@hotmail.com>
2016-12-09 21:13:41 +0100

    Mean stack

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module('flapperNews', []);
+angular.module('flapperNews', ['ui.router'])
+
+
+
+app.factory('posts', [function(){
+	var o = {
+    posts: []
+  };
+  return o;
+}])
+
+
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
+	$scope.posts = posts.posts;
+
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === '')
+		{
+			return;
+		}
+	  $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+	  $scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1
+	};
+
+}]);
