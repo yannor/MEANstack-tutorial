5428f81508d24c402c6871c6632943fe0f793ac8
Yannick <yannick.servranckx@hotmail.com>
2016-12-11 22:59:27 +0100

    Tutorial almost done

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,3 +1,8 @@
+
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
---
+++ flapper-news/models/Posts.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+    title: String,
+    link: String,
+    upvotes: {
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+});
+
+mongoose.model('Post', PostSchema);
+
+PostSchema.methods.upvote = function(cb) {
+    this.upvotes += 1;
+    this.save(cb);
+};
+
+PostSchema.methods.upvote = function(cb) {
+    this.upvotes += 1;
+    this.save(cb);
+};
