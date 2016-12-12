a4b0ce7b3d2d8aaf1186f898f201360710a35267
Yannick <yannick.servranckx@hotmail.com>
2016-12-12 22:56:52 +0100

    Fixes

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -1,8 +1,3 @@
-
-var mongoose = require('mongoose');
-require('./models/Posts');
-require('./models/Comments');
-mongoose.connect('mongodb://localhost/news');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -10,42 +5,74 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+// connect MongoDB
+mongoose.connect('mongodb://localhost/news', function(err,db){
+    if (!err){
+        console.log('Connected to /news!');
+    } else{
+        console.dir(err); //failed to connect
+    }
+});
+
+require('./models/Posts');
+require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
