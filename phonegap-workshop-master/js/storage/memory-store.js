dd69680314de94637fb51b14cba66ed38ae124f7
Yannick <yannick.servranckx@hotmail.com>
2016-12-21 17:30:35 +0100

    Added employees

--- phonegap-workshop-master/js/storage/memory-store.js
+++ phonegap-workshop-master/js/storage/memory-store.js
@@ -46,7 +46,11 @@ var MemoryStore = function(successCallback, errorCallback) {
             {"id": 12, "firstName": "Stanley", "lastName": "Hudson", "title":"Sales Representative", "managerId": 10, "city":"Scranton, PA", "cellPhone":"570-700-6464", "officePhone":"570-787-9393", "email":"shudson@dundermifflin.com"},
             {"id": 13, "firstName": "Meredith", "lastName": "Palmer", "title":"Supplier Relations", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-588-6567", "officePhone":"570-981-6167", "email":"meredith@dundermifflin.com"},
             {"id": 14, "firstName": "Kelly", "lastName": "Kapoor", "title":"Customer Service Rep.", "managerId": 2, "city":"Scranton, PA", "cellPhone":"570-123-9654", "officePhone":"570-125-3666", "email":"kelly@dundermifflin.com"},
-            {"id": 15, "firstName": "Toby", "lastName": "Flenderson", "title":"Human Resources", "managerId": 1, "city":"Scranton, PA", "cellPhone":"570-485-8554", "officePhone":"570-699-5577", "email":"toby@dundermifflin.com"}
+            {"id": 15, "firstName": "Toby", "lastName": "Flenderson", "title":"Human Resources", "managerId": 1, "city":"Scranton, PA", "cellPhone":"570-485-8554", "officePhone":"570-699-5577", "email":"toby@dundermifflin.com"},
+            {"id": 16, "firstName": "Yannick", "lastName": "Servranckx", "title":"Student HoGent", "managerId": 0, "city":"Aalst, Belgie", "cellPhone":"0478/35.81.69", "officePhone":"/", "email":"yannick.servranckx.t3539@student.hogent.be"},
+            {"id": 17, "firstName": "Pieter", "lastName": "Van Der Helst", "title":"Lector Webapps HoGent", "managerId": 0, "city":"Gent, Belgie", "cellPhone":"/", "officePhone":"/", "email":"pieter.vanderhelst@hogent.be"}
+
+
         ];
 
     callLater(successCallback);
