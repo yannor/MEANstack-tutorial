be454def6e19ac0cb5e9901bafa11ba077d7f17b
Yannick <yannick.servranckx@hotmail.com>
2016-12-21 17:33:54 +0100

    Add to contacts

--- phonegap-workshop-master/js/EmployeeView.js
+++ phonegap-workshop-master/js/EmployeeView.js
@@ -7,7 +7,42 @@ var EmployeeView = function(employee) {
 
     this.render = function() {
         this.el.html(EmployeeView.template(employee));
+        //button voor add location
+        this.el.on('click', '.add-location-btn', this.addLocation);
+        //button voor add to contacts
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
         return this;
+    };
+
+// Shows the location of employee (in latitude en longitude)
+    this.addLocation = function(event) {
+        event.preventDefault();
+        console.log('addLocation');
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+            },
+            function() {
+                alert('Error getting location');
+            });
+        return false;
+    };
+
+    this.addToContacts = function(event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
+        return false;
     };
 
     this.initialize();
