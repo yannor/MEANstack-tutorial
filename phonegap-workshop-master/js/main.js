ae2afd90875cfb45bc13ee126ca3ea64fd6e0fb8
Yannick <yannick.servranckx@hotmail.com>
2016-12-21 17:07:26 +0100

    Templates and CSS

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,21 +1,44 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+  registerEvents: function() {
+      var self = this;
+      // Check of browser supports touch events...
+      if (document.documentElement.hasOwnProperty('ontouchstart')) {
+          // ... if yes: register touch event listener to change the "selected" state of the item
+          $('body').on('touchstart', 'a', function(event) {
+              $(event.target).addClass('tappable-active');
+          });
+          $('body').on('touchend', 'a', function(event) {
+              $(event.target).removeClass('tappable-active');
+          });
+      } else {
+          // ... if not: register mouse events instead
+          $('body').on('mousedown', 'a', function(event) {
+              $(event.target).addClass('tappable-active');
+          });
+          $('body').on('mouseup', 'a', function(event) {
+              $(event.target).removeClass('tappable-active');
+          });
+      }
+  },
+
+    showAlert: function(message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
+
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        var self = this;
+
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+        self.registerEvents();
+
     }
 
 };
