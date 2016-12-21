09b5907dfa293783d6c133a19722ed7ae6e0711a
Yannick <yannick.servranckx@hotmail.com>
2016-12-21 18:02:37 +0100

    Wrapping up

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -48,6 +48,45 @@ var app = {
         }
     },
 
+    slidePage: function(page) {
+
+    var currentPageDest,
+        self = this;
+
+    // If there is no current page (app just started) -> No transition: Position new page in the view port
+    if (!this.currentPage) {
+        $(page.el).attr('class', 'page stage-center');
+        $('body').append(page.el);
+        this.currentPage = page;
+        return;
+    }
+
+    // Cleaning up: remove old pages that were moved out of the viewport
+    $('.stage-right, .stage-left').not('.homePage').remove();
+
+    if (page === app.homePage) {
+        // Always apply a Back transition (slide from left) when we go back to the search page
+        $(page.el).attr('class', 'page stage-left');
+        currentPageDest = "stage-right";
+    } else {
+        // Forward transition (slide from right)
+        $(page.el).attr('class', 'page stage-right');
+        currentPageDest = "stage-left";
+    }
+
+    $('body').append(page.el);
+
+    // Wait until the new page has been added to the DOM...
+    setTimeout(function() {
+        // Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
+        $(self.currentPage.el).attr('class', 'page transition ' + currentPageDest);
+        // Slide in the new page
+        $(page.el).attr('class', 'page stage-center transition');
+        self.currentPage = page;
+    });
+
+},
+
     initialize: function() {
     var self = this;
     this.detailsURL = /^#employees\/(\d{1,})/;
